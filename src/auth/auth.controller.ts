799bae2b4d8e295c187dcd1ac7e6a598dbfecdda
bastian <bastiangarcesga@gmail.com>
2023-03-22 21:15:30 -0300

    create login auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AgencyRegistrationDto } from '../users/agency/dto/agency-registration.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() agencyRegistrationDto: AgencyRegistrationDto) {
+    const agency = await this.authService.validateAgency(
+      agencyRegistrationDto.email,
+      agencyRegistrationDto.password,
+    );
+    return this.authService.login(agency);
+  }
+}
