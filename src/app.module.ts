577732b5d2ef02cfa5b4ce43017ef7b0cfcdd4f9
Tomás <tomasarreguib@gmail.com>
2023-03-25 03:37:22 -0300

    FIX: deleted unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,45 +1,26 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AgencyModule } from './users/agency/agency.module';
-import { BannerController } from './banner/banner.controller';
-import { BannerService } from './banner/banner.service';
 import { BannerModule } from './banner/banner.module';
-import { SellerController } from './seller/seller.controller';
 import { SellerModule } from './seller/seller.module';
-import { TripController } from './trip/trip.controller';
-import { TripService } from './trip/trip.service';
 import { TripModule } from './trip/trip.module';
-import { RoleController } from './role/role.controller';
-import { RoleService } from './role/role.service';
 import { RoleModule } from './role/role.module';
-import { AdminController } from './admin/admin.controller';
-import { AdminService } from './admin/admin.service';
 import { AdminModule } from './admin/admin.module';
 import { AgencyUserModule } from './agency-user/agency-user.module';
-import { InvitationController } from './invitation/invitation.controller';
-import { InvitationService } from './invitation/invitation.service';
 import { InvitationModule } from './invitation/invitation.module';
-import { ChatController } from './chat/chat.controller';
-import { ChatService } from './chat/chat.service';
 import { ChatModule } from './chat/chat.module';
-import { TouristController } from './tourist/tourist.controller';
-import { TouristService } from './tourist/tourist.service';
 import { TouristModule } from './tourist/tourist.module';
-import { ClaimController } from './claim/claim.controller';
-import { ClaimService } from './claim/claim.service';
 import { ClaimModule } from './claim/claim.module';
-import { ResponsibleController } from './responsible/responsible.controller';
 import { ResponsibleModule } from './responsible/responsible.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-      isGlobal: true,
+      isGlobal: true
     }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     AgencyModule,
@@ -54,10 +35,10 @@ import { AuthModule } from './auth/auth.module';
     TouristModule,
     ClaimModule,
     ResponsibleModule,
-    AuthModule,
+    AuthModule
   ],
 
   controllers: [],
-  providers: [],
+  providers: []
 })
 export class AppModule {}
