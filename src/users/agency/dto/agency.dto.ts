320bef388f3d8f96b2c9564efed84574f2a50f30
Tomás <tomasarreguib@gmail.com>
2023-03-27 01:05:13 -0300

    ADD: dto for agency and global pipe validation

---
+++ src/users/agency/dto/agency.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class AgencyRegistrationDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phoneNumber: string;
+}
