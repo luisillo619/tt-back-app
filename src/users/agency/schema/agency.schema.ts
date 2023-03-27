55207d731311d5f179df4a82776f50f39949eafe
Tomás <tomasarreguib@gmail.com>
2023-03-27 01:01:49 -0300

    FIX: linter

--- src/users/agency/schema/agency.schema.ts
+++ src/users/agency/schema/agency.schema.ts
@@ -1,8 +1,10 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Prop } from '@nestjs/mongoose/dist';
-import mongoose, { ObjectId, Document } from 'mongoose';
-import { AgencyUser } from 'src/agency-user/schema/agency-user.schema';
 import { HydratedDocument } from 'mongoose';
+
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+import { AgencyUser } from 'src/agency-user/schema/agency-user.schema';
+
+import { ObjectId } from '../utils/database.utils';
 
 export type AgencyDocument = HydratedDocument<Agency>;
 
@@ -10,32 +12,46 @@ export type AgencyDocument = HydratedDocument<Agency>;
 export class Agency {
   @Prop()
   name: string;
+
   @Prop({ unique: true })
   email: string;
+
   @Prop({ required: true })
   password: string;
+
   @Prop()
   phoneNumber: number;
+
   @Prop()
   legalTax: string;
+
   @Prop()
   isActive: boolean;
+
   @Prop()
   AgencyPayment: string[];
+
   @Prop()
   planType: string;
+
   @Prop()
   guide: string;
+
   @Prop()
   responsable: string;
+
   @Prop()
   banners: string[];
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'AgencyUser' }] })
+
+  @Prop({ type: [{ type: ObjectId, ref: 'AgencyUser' }] })
   agencyUsers: AgencyUser[];
+
   @Prop()
   sellers: string[];
+
   @Prop()
   invitationSended: string[];
+
   @Prop()
   trip: string[];
 }
